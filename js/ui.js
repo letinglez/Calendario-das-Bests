46c8564887d0def9c0b1984978392a8a125b6666
letinglez <leticia.martines@hotmail.com>
2026-07-22 11:49:39 -0300

    Fix syntax and add functionality in ui.js

--- js/ui.js
+++ js/ui.js
@@ -4,10 +4,12 @@ const aplicacao = document.getElementById("aplicacao");
 const saudacao = document.getElementById("saudacao");
 
 function entrar(pessoa) {
+
     saudacao.textContent = `Olá, ${pessoa.nome}! ${pessoa.coracao}`;
 
     telaInicial.classList.add("oculto");
     aplicacao.classList.remove("oculto");
+
 }
 
 function carregarAmigas() {
@@ -19,8 +21,6 @@ function carregarAmigas() {
 
     pessoas.forEach(pessoa => {
 
-}
-
         const botao = document.createElement("button");
 
         botao.className = "amiga";
@@ -28,7 +28,9 @@ function carregarAmigas() {
         botao.innerHTML = `${pessoa.coracao} ${pessoa.nome}`;
 
         botao.addEventListener("click", () => {
+
             entrar(pessoa);
+
         });
 
         listaAmigas.appendChild(botao);
