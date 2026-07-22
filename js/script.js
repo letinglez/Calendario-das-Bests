dd7c03bf273ac3976358b7cff3ad294a021e775b
letinglez <leticia.martines@hotmail.com>
2026-07-22 11:23:44 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 console.log("Calendário iniciado!");
+
+const telaInicial = document.getElementById("telaInicial");
+const aplicacao = document.getElementById("aplicacao");
+const saudacao = document.getElementById("saudacao");
+
+const botoes = document.querySelectorAll(".amiga");
+
+function entrar(nome){
+
+    saudacao.textContent = `Olá, ${nome}!`;
+
+    telaInicial.classList.add("oculto");
+
+    aplicacao.classList.remove("oculto");
+
+}
+
+botoes.forEach(botao=>{
+
+    botao.addEventListener("click",()=>{
+
+        entrar(botao.dataset.nome);
+
+    });
+
+});
