42d360f70fdea7da08a9f8a2b3342cc05b81bc6a
letinglez <leticia.martines@hotmail.com>
2026-07-22 13:28:07 -0300

    Implement month display in calendario.js
    Add functionality to display the current month and year.

--- js/calendario.js
+++ js/calendario.js
@@ -1,3 +1,27 @@
+const hoje = new Date();
+
+let mesAtual = hoje.getMonth();
+let anoAtual = hoje.getFullYear();
+
+const nomesMeses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+];
+
 function mostrarMes() {
+
+    const titulo = document.getElementById("mesAtual");
+
+    titulo.textContent = `${nomesMeses[mesAtual]} ${anoAtual}`;
 
 }
