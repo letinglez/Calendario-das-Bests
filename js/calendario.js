70e7e7e080caba4223c84b05d341e4f1767c31e2
letinglez <leticia.martines@hotmail.com>
2026-07-22 13:08:42 -0300

    Update calendario.js

--- js/calendario.js
+++ js/calendario.js
@@ -1,1 +1,15 @@
+const hoje = new Date();
 
+let mesAtual = hoje.getMonth();
+
+let anoAtual = hoje.getFullYear();
+
+gerarDias()
+
+trocarMes()
+
+const hoje = new Date();
+
+let mesAtual = hoje.getMonth();
+
+let anoAtual = hoje.getFullYear();
