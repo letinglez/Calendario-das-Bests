b0e92ae1b873ea15d93bd258f8435338bbc69600
letinglez <leticia.martines@hotmail.com>
2026-07-23 21:03:19 -0300

    Initialize Firebase app and export database functions

--- js/firebase.js
+++ js/firebase.js
@@ -3,3 +3,27 @@ agenda = {
   "2026-7-24": ["🩷"],
   "2026-8-01": ["💙", "❤️", "💚"]
 }
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+    getDatabase,
+    ref,
+    set,
+    onValue
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPLVbZt0e-_urBBGXU6Nr_HknUMs8w6R0",
+    authDomain: "agenda-de-encontros.firebaseapp.com",
+    databaseURL: "https://agenda-de-encontros-default-rtdb.firebaseio.com",
+    projectId: "agenda-de-encontros",
+    storageBucket: "agenda-de-encontros.firebasestorage.app",
+    messagingSenderId: "97670253088",
+    appId: "1:97670253088:web:6702dff70b0c1d57b68a45"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+export { database, ref, set, onValue };
